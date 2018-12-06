9066443495a86bb400252a34041d9e848bf08219
xwwx <13452010@qq.com>
2018-12-06 16:02:57 +0800

    设计模式

--- srp/src/main/java/com/xwwx/design/activity/MainActivity.java
+++ srp/src/main/java/com/xwwx/design/activity/MainActivity.java
@@ -3,13 +3,11 @@ package com.xwwx.design.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.xwwx.design.R;
 import com.xwwx.design.common.DoubleCache;
 import com.xwwx.design.common.ImageLoader;
-import com.xwwx.design.common.MemoryCache;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadImage(View view){
-        String url = "https://avatar.csdn.net/E/5/4/3_wuqilianga.jpg";
+        String url = "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2386734369,3655898069&fm=179&app=42&f=PNG?w=121&h=140";
 
         imageLoader.setmImageCache(doubleCache);
         imageLoader.displayImage(url,imgv_main_show);
